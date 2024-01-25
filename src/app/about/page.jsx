ee358ab2cd9677084255325bf32774576d746464
Nikhil Vishwakarma <122264427+Nikhilv47@users.noreply.github.com>
2024-01-25 07:31:11 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className=' max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet incidunt aliquid dolor mollitia, eveniet asperiores ut, repellendus illo placeat, provident dolorem. Fugit harum quisquam iure!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, neque corporis? Adipisci voluptas architecto asperiores facere eaque? Omnis eum eveniet similique atque facilis reiciendis laborum, tempora voluptatem accusamus libero dolorem minus obcaecati aperiam nihil distinctio dolore, perspiciatis dolorum provident quia.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores assumenda dolores ratione voluptates quasi ab error itaque quos laborum debitis optio, eos accusamus sequi nisi voluptatem a aperiam vel fugit! Velit culpa placeat architecto cumque magni error consequuntur aut aliquid facilis delectus sapiente, nemo ex expedita autem, nisi nihil.
+      </p>
+    </div>
+  )
+}
